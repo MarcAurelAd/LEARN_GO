1c728a629e40e0a7cbc3896f4ee023475e756422
MarcAurelAd <marcaurelalladassi@gmail.com>
2025-09-07 14:05:29 +0100

    Newfeature : Compte le nombre de consonnes et de voyelles/ Réécrit l'input en majuscule et miniscule

--- Exercices/analysedetexte.go
+++ Exercices/analysedetexte.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -18,5 +19,42 @@ func main() {
 	}
 
 	longueur := len(phrase)
-	fmt.Println("Votre phrase contient ", longueur, "caractères.")
+
+	//convertit la phrase en une suite de caractères, un caractère par mot dans la phrase initiale
+	mots := strings.Fields(phrase)
+
+	// Supprimer les espaces blancs autour de la chaîne de caractères (y compris le retour à la ligne)
+	phrase = strings.TrimSpace(phrase)
+
+	voyelles := "aeéèêïëiouyAEIOUY"
+	nombreVoyelles := 0
+
+	// Parcourir chaque caractère de l'input
+	for _, char := range phrase {
+		// Vérifier si le caractère est présent dans la chaîne des voyelles
+		if strings.ContainsRune(voyelles, char) {
+			nombreVoyelles++
+		}
+
+	}
+
+	consonnes := "zrtpqsdghfjklmwxcvbnZRTPQSDFGHJKLMWXCVBN"
+	nombreConsonnes := 0
+
+	// Parcourir chaque caractère de l'input
+	for _, char := range phrase {
+		// Vérifier si le caractère est présent dans la chaîne des voyelles
+		if strings.ContainsRune(consonnes, char) {
+			nombreConsonnes++
+		}
+
+	}
+	fmt.Println("Votre phrase contient ", longueur, "caractères, ", len(mots), "mots, ", nombreVoyelles, " voyelles et", nombreConsonnes, "consonnes.")
+
+	//passe l'input en majuscule/miniscule
+	phraseMaj := strings.ToUpper(phrase)
+	phraseMin := strings.ToLower(phrase)
+
+	fmt.Println("Vous avez entré\n", phraseMaj, "\n", phraseMin)
+
 }
